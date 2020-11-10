38c6a59d2b695c23ca1258fe985b264db3c161e1
gabrielpaiva2019 <46344395+gabrielpaiva2019@users.noreply.github.com>
2020-11-10 20:54:15 -0300

    teste

---
+++ src/test/java/com/cardocs/api/CarDocsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cardocs.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarDocsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
