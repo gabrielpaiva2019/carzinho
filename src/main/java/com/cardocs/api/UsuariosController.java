b298392eadbdc4c7967552e461609b4a52a433a3
gabrielpaiva2019 <46344395+gabrielpaiva2019@users.noreply.github.com>
2020-11-10 21:05:13 -0300

    teste

--- src/main/java/com/cardocs/api/UsuariosController.java
+++ src/main/java/com/cardocs/api/UsuariosController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UsuariosController {
 
-    @GetMapping
+    @GetMapping("/home")
     public String index(){
         return "Hello world";
-        
+
     }
 }
